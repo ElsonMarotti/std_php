9634ba371aac408b0a7a37e45c4f62140d1322dd
ElsonMarotti <elsonmarotti.sist2011@hotmail.com>
2024-07-12 20:38:55 -0300

    Arquivo php de estudo pratico com code of math, resolução de quest

---
+++ var_dump2.php
@@ -0,0 +1,12 @@
+<!--
+    Exercicio de prova de concurso.
+  > Teste de mesa para lembra do uso de atribuição pra cálculo ".", visto junto com MArco
+!-->
+<?php
+
+$x = "10";
+echo var_dump($x . "1" + 5.2 . $x * 2 + 5);
+?>
+
+$x . "1" + 5.2.$x*2+5;
+10."1" + 5.2.10*2+5;
